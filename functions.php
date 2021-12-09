45f3cf864013d11ec2165e7874a2dc907a892728
pern7295 <pern7295@stud.kea.dk>
2021-12-09 12:44:42 +0100

    fjernet php

--- functions.php
+++ functions.php
@@ -15,15 +15,3 @@ endif;
 add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
 
 // END ENQUEUE PARENT ACTION
-
-// To change add to cart text on single product page
-add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' ); 
-function woocommerce_custom_single_add_to_cart_text() {
-    return __( 'Læs mere', 'woocommerce' ); 
-}
-
-// To change add to cart text on product archives(Collection) page
-add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );  
-function woocommerce_custom_product_add_to_cart_text() {
-    return __( 'Læs mere', 'woocommerce' );
-}
