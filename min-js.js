4e9607996a525cb68e1c9fdb89e0b8f9209f840c
pern7295 <pern7295@stud.kea.dk>
2021-12-10 17:28:31 +0100

    gsap

--- min-js.js
+++ min-js.js
@@ -1,5 +1,32 @@
 console.log("Custom JS virker!");
 
+gsap.registerPlugin(ScrollTrigger);
+
+const containers = document.querySelectorAll("h2");
+
+containers.forEach((container) => {
+  const h2s = container.querySelector("gsapTry");
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: container,
+        start: "top 50%",
+        end: "+=200",
+        scrub: true,
+        //pin: true,
+        // markers: true,
+      },
+    })
+
+    .from(h2s, {
+      duration: 2,
+      opacity: 0,
+    });
+});
+
+console.log("GSAP VIRKER");
+
 document.querySelector(".jazz_btn").addEventListener("click", clickCategori);
 
 function clickCategori() {
