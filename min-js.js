2479da7ceae5a6504a7db0eae50f917dfb87c98a
pern7295 <pern7295@stud.kea.dk>
2021-12-10 16:28:25 +0100

    en masse fix

--- min-js.js
+++ min-js.js
@@ -1,1 +1,8 @@
 console.log("Custom JS virker!");
+
+document.querySelector(".jazz_btn").addEventListener("click", clickCategori);
+
+function clickCategori() {
+  document.querySelector(".jazz").classList.toggle("jazz_display");
+}
+console.log("Knap virker");
