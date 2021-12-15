f4e95045d7145f0ed6089a6cafc51a98f398665a
kasp041w <kasp041w@stud.kea.dk>
2021-12-15 13:36:46 +0100

    Javascript på knapper landingpage bund

--- min-js.js
+++ min-js.js
@@ -1,1 +1,18 @@
 console.log("Custom JS virker!");
+
+
+
+document.querySelector(".jatak_tilmeld_knap").addEventListener("mousedown", jatak);
+document.querySelector(".maaske_tilmeld_knap").addEventListener("mousedown", jamaaske);
+
+function jatak(){
+    document.querySelector(".tilmeld_jatak_js").classList.toggle("display_none");
+    document.querySelector(".tilmeld_maaske_js").classList.add("display_none");
+    
+}
+
+function jamaaske(){
+    document.querySelector(".tilmeld_maaske_js").classList.toggle("display_none");
+    document.querySelector(".tilmeld_jatak_js").classList.add("display_none");
+
+}
